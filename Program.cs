df8e2761ca4b80cb5df8793e0e5331d92ece93fe
Daniel Svensson <danielsvenssonn@gmail.com>
2023-08-23 20:37:37 +0200

    Added CORS policy to allow-any-origin, needed when both the react app and the api is using localhost.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => { options.AddPolicy("CorsPolicy", policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); }); });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
