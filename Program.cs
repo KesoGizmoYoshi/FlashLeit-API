017adb2d1e76728620945e1f7944d90a04c1a0cc
Daniel Svensson <danielsvenssonn@gmail.com>
2023-08-28 15:24:14 +0200

    Re-added CORS-policy, for full local development

--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
-using Azure.Core;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using FlashLeit_API.Data.Database;
 using FlashLeit_API.DataAccess;
 using FlashLeit_API.Repositories.Implementations;
 using FlashLeit_API.Repositories.Interfaces;
 using FlashLeit_API.Services;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>();
 
-
 builder.Services.AddControllers();
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +20,7 @@ builder.Services.AddSingleton<IConnectionStringService, ConnectionStringService>
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddSingleton<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddCors(options => { options.AddPolicy("CorsPolicy", policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); }); });
 
 var app = builder.Build();
 
