53b2ee22d8f16d30f8ae2794f1ee1b228e5ac6ab
Daniel Svensson <danielsvenssonn@gmail.com>
2023-08-28 14:47:46 +0200

    Added POST for the UsersController and the required models for B2C integration.

---
+++ Models/B2CRelatedModels/FailedValidationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashLeit_API.Models.B2CRelatedModels;
+
+public class FailedValidationModel
+{
+    public string? Version { get; set; } = "1.0.0";
+    public string? Status { get; set; } = "400";
+    public string? Action { get; set; } = "ValidationError";
+    public string? UserMessage { get; set; } = "Username is already in use!"; 
+}
