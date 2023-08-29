f58f0ec2f816a81d68aef44eb4bd799abd8f1bb3
Daniel Svensson <danielsvenssonn@gmail.com>
2023-08-29 12:27:41 +0200

    First Attempt to use built-in Claim Postal Code!

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -62,7 +62,7 @@ public class UsersController : ControllerBase
 
             var userModelId = newUser.FirstOrDefault().Id;
 
-            successfulValidation.Extension_UserId = userModelId.ToString();
+            successfulValidation.PostalCode = userModelId.ToString();
 
             response = new(new { successfulValidation });
 
