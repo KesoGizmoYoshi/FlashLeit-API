5150be9201689a3346298e54c40f840d3ff2baea
Daniel Svensson <danielsvenssonn@gmail.com>
2023-09-08 15:30:27 +0200

    Azure related comments added for ConnectionStringService

--- Services/ConnecionStringService.cs
+++ Services/ConnecionStringService.cs
@@ -9,6 +9,16 @@ public class ConnectionStringService : IConnectionStringService
     private readonly SecretClient _client;
     private readonly SecretClientOptions _options;
 
+    // The SecretClient is used for accessing a specified Azure Key Vault.
+    // Only options regarding Retry is defined for the SecretClient.
+    // Azure Key Vault offers many different ways to authenticate user/application access.
+    // The default method is DefaultAzureCredential(), because it tries every available auth method.
+    // This is a very slow method to use during development. But really fast, if the application is deployed in Azure.
+    // So we opted to use ChainedTokenCredential() which allow us to decide which auth methods that should be used.
+    // This also lets us decide in which order the auth methods will be used.
+    // So in our case SecretClient will use AzureCliCredential() first, this is by far the fastest auth method for local development.
+    // Only requires that the user login with Azure Cli. Command: "az login"
+    // And ManagedIdentityCredential() is the auth method that is used when the application is deployed in Azure.
     public ConnectionStringService()
     {
         _options = new SecretClientOptions()
@@ -25,6 +35,11 @@ public class ConnectionStringService : IConnectionStringService
         _client = new SecretClient(new Uri("https://flashleit-keys.vault.azure.net/"), 
             new ChainedTokenCredential(new AzureCliCredential(), new ManagedIdentityCredential()), _options);
     }
+
+    // This method uses the initialized SecretClient to access our Azure Key Vault.
+    // By using the GetSecret() method, we can obtain any secrets safely stored in Azure Key Vault.
+    // The required parameter for GetSecret() is whatever name you gave it, when the secret was created.
+    // Finally the method returns our connection string.
     public string GetConnectionStringFromAzureKeyVault()
     {
         KeyVaultSecret connectionString = _client.GetSecret("ConnectionString--FlashleitDbConnection");
