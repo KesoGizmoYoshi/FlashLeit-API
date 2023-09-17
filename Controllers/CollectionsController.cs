1b2eb3874156f632f5439dfe4b507d37057f1d01
Benjamin Bonnerup <112319740+Halfd4n@users.noreply.github.com>
2023-09-17 10:44:22 +0200

    Try just Id again

--- Controllers/CollectionsController.cs
+++ Controllers/CollectionsController.cs
@@ -68,7 +68,7 @@ public class CollectionsController : ControllerBase
 
             
 
-            return Ok(results.FirstOrDefault());
+            return Ok(results.FirstOrDefault().Id);
         }
 
         return BadRequest();
